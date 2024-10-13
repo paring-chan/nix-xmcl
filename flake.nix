858d233a0ac120988fe38777e8e490000d81b4f7
파링 <me@paring.moe>
2024-10-14 00:29:58 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      packages.${system} = {
+        xmcl = pkgs.callPackage ./package.nix { };
+        default = self.packages.x86_64-linux.xmcl;
+      };
+
+    };
+}
---
+++ package.nix
@@ -0,0 +1,37 @@
+{
+  appimageTools,
+  fetchurl,
+  lib,
+}:
+let
+  pname = "xmcl";
+  version = "0.47.4";
+  src = fetchurl {
+    url = "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x86_64.AppImage";
+    hash = "sha256-ZOJ05MLN8zWPywcxoMDiIjlS0Dec2M7MYNG95VOLY70=";
+  };
+  appimageContents = appimageTools.extractType2 { inherit pname src version; };
+  meta = with lib; {
+    description = "An Open Source Minecraft Launcher with Modern UX.";
+    homepage = "https://xmcl.app";
+    license = licenses.mit;
+    mainProgram = "xmcl";
+
+    platforms = [ "x86_64-linux" ];
+  };
+in
+appimageTools.wrapType2 {
+  inherit
+    pname
+    version
+    src
+    meta
+    ;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Exec=AppRun' 'Exec=xmcl'
+    cp -r ${appimageContents}/usr/share/icons $out/share
+  '';
+}
