de5e2307cac673937b3fcaf4369166a57eaa99ab
paring <me@paring.moe>
2024-12-21 16:19:22 +0900

    feat: use system electron

--- package.nix
+++ package.nix
@@ -1,16 +1,19 @@
 {
-  appimageTools,
+  stdenv,
   fetchurl,
   lib,
+  electron_33,
+  bash,
+  makeDesktopItem
 }:
 let
   pname = "xmcl";
-  version = "0.47.10";
+  version = "0.48.1";
   src = fetchurl {
-    url = "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x86_64.AppImage";
-    hash = "sha256-6nEzjokcb4y7/etKiXPmgFO8VgRGk+lIQkaDszaAaRI=";
+    url = "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x64.tar.xz";
+    hash = "sha256-y/Nx02ZO9V9wBJaZf5GDvPDInm2Ifxh9boZ7U9K4adY=";
   };
-  appimageContents = appimageTools.extractType2 { inherit pname src version; };
+  # appimageContents = appimageTools.extractType2 { inherit pname src version; };
   meta = with lib; {
     description = "An Open Source Minecraft Launcher with Modern UX.";
     homepage = "https://xmcl.app";
@@ -19,19 +22,44 @@ let
 
     platforms = [ "x86_64-linux" ];
   };
+  desktopEntry = makeDesktopItem {
+    name = "xmcl";
+    desktopName = "X Minecraft Launcher";
+    exec = "xmcl %f";
+    terminal = false;
+  };
 in
-appimageTools.wrapType2 {
-  inherit
-    pname
-    version
-    src
-    meta
-    ;
+stdenv.mkDerivation {
+  inherit src pname version;
+  buildInputs = [
+    electron_33
+  ];
 
-  extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace 'Exec=AppRun' 'Exec=xmcl'
-    cp -r ${appimageContents}/usr/share/icons $out/share
+  installPhase = ''
+    mkdir -p $out/etc/xmcl
+    mkdir -p $out/share/applications
+    cp resources/app.asar $out/etc/xmcl/app.asar
+    mkdir -p $out/bin
+    echo '#!${bash}/bin/bash' > $out/bin/xmcl
+    echo "${electron_33}/bin/electron $out/etc/xmcl/app.asar \$@" >> $out/bin/xmcl
+    cp ${desktopEntry}/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
+    chmod +x $out/bin/xmcl
   '';
 }
+/*
+  appimageTools.wrapType2 {
+    inherit
+      pname
+      version
+      src
+      meta
+      ;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/${pname}.desktop $out/share/applications/${pname}.desktop
+      substituteInPlace $out/share/applications/${pname}.desktop \
+        --replace 'Exec=AppRun' 'Exec=xmcl'
+      cp -r ${appimageContents}/usr/share/icons $out/share
+    '';
+  }
+*/
